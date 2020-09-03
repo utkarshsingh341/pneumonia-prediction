88745af59ea7190e8cc7557d13d10b2f3fdff5e2
Utkarsh <utkarshsingh341@gmail.com>
2020-09-03 13:58:36 +0530

    Upload files

---
+++ demo3.py
@@ -0,0 +1,20 @@
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
+import numpy as np
+
+model = load_model('model_resnet.h5')
+img = image.load_img('Datasets/val/PNEUMONIA/person1947_bacteria_4876.jpeg',target_size=(224,224))
+#img = image.load_img('Datasets/val/NORMAL/NORMAL2-IM-1442-0001.jpeg',target_size=(224,224))
+x = image.img_to_array(img)
+x = np.expand_dims(x,axis=0)
+img_data = preprocess_input(x)
+classes = model.predict(img_data)
+
+print("\n#########OUTPUT#########\n")
+print("\nClass Array: ",classes)
+x = classes[0]
+if(x[0]>x[1]):
+    print("\nThe person is normal\n")
+else:
+    print("\nThe person has pneumonia\n")
